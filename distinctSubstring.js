91c629233d4589d484af037366cabd65fc4ef22a
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-05-05 23:53:11 +0530

    distinctSubstring

---
+++ distinctSubstring.js
@@ -0,0 +1,15 @@
+function distinctSubstrings(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        let sub = new Set();
+        for (let j = i; j < str.length; j++) {
+            if (sub.has(str[j])) {
+                break;
+            }
+            sub.add(str[j])
+            console.log(sub)
+            count++
+        }
+    }
+    console.log(count);
+}
